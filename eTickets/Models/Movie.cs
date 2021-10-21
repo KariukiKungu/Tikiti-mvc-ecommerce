fdc5535affd8f17592d8a15d82d5daed2473af8b
KariukiKungu <67824027+KariukiKungu@users.noreply.github.com>
2021-10-21 17:19:00 +0300

    Add project files.

---
+++ eTickets/Models/Movie.cs
@@ -0,0 +1,21 @@
+﻿using eTickets.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eTickets.Models
+{
+    public class Movie
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+        public double ImageURL { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public MovieCategory MovieCategory { get; set; }
+    }
+}
